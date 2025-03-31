cf9074fc111a5114355679e915534c1334e2327b
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-03-31 17:41:14 +0100

    search filters main form

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Form, useLoaderData, Link } from 'react-router-dom'
+import FormInput from './FormInput'
 
-function Filters() {
+const Filters = () => {
+  // get req so we do not add anything
+  // ... when we submit the query params get added to the get string
   return (
-    <div>Filters</div>
+    <Form className='bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
+      {/* SEARCH */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        size='input-sm'
+      />
+      {/* BUTTONS */}
+      <button type='submit' className='btn btn-primary btn-sm '>
+        search
+      </button>
+      {/* navigate back to the same page  */}
+      <Link to='/products' className='btn btn-accent btn-sm'>
+        reset
+      </Link>
+    </Form>
   )
 }
-
 export default Filters
